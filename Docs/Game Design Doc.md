c489fd9adf03fc68aaf731d04b6bd676d8330cd8
OmerHakanKilic <omerhakankilic04@gmail.com>
2026-06-28 14:12:56 +0100

    Tick implementation

---
+++ Docs/Game Design Doc.md
@@ -0,0 +1,8 @@
+# Game Pitch
+Terminal based incremental game like Vic 3 set in space. Starting from a settlement potentially expanding to control multiple settlements.
+
+# Design Pillars
+* Decision making
+* Number goes up
+* Diversity of decisions
+
--- main.c
+++ main.c
@@ -1,3 +1,23 @@
 #include <ncurses.h>
+#include <unistd.h>
 
-int main() { return 0; }
+int main() {
+  initscr();
+  cbreak();
+  noecho();
+  curs_set(0);
+  timeout(0);
+
+  int ch;
+  int counter = 0;
+  while (1) {
+    clear();
+    ch = getch();
+    counter += 1;
+    printw("Char: %c Counter: %d", ch, counter);
+    refresh();
+    usleep(1000000);
+  }
+  endwin();
+  return 0;
+}
